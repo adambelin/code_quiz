b49ce99776c3db7edbc7f29c25d2d2ebe6ec1ac9
Adam Belin <adambelin1992@gmail.com>
2022-03-05 22:19:11 -0500

    added quiz functionality in JS

--- develop/script.js
+++ develop/script.js
@@ -48,8 +48,52 @@ let myQuestions = [
     },
 ]
 
-function startQuiz(){
+function answerQuestion(answer, correctAnswer){
+    if (answer===correctAnswer) {
+        alert("correct dumbass");
+    }
+    else {
+        alert("quit being stupid")
+    }
+    console.log(answer);
+    myQuestions.shift ();
+    nextQuestion();
+}
+
+function startQuiz() {
+    nextQuestion();
+}
+
+function endQuiz() {
+    alert("good job pp head")
+}
+
+function nextQuestion(){
     $("#rules").addClass("hidden");
+    $("#question").removeClass("hidden");
+    if (myQuestions.length === 0) {
+        endQuiz();
+        return;
+    }
+    let question=myQuestions [0];
+    $("#question_title").html(question.question);
+    $("#answer_a").html(question.answers.a);
+    $("#answer_b").html(question.answers.b);
+    $("#answer_c").html(question.answers.c);
+    $("#answer_d").html(question.answers.d);
+
+    $ ("#answer_a").off("click").click(function (){
+        answerQuestion("a", question.correctAnswer);
+    });
+    $ ("#answer_b").off("click").click(function (){
+        answerQuestion("b", question.correctAnswer);
+    });
+    $ ("#answer_c").off("click").click(function (){
+        answerQuestion("c", question.correctAnswer);
+    });
+    $ ("#answer_d").off("click").click(function (){
+        answerQuestion("d", question.correctAnswer);
+    });
 }
 
 $ ("#start").click(startQuiz);
